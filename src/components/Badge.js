28d05a06bffcafe33f22fa8d9a9fdb9b8d582a4e
Mariangelica Useche <mariangelica.useche@gmail.com>
2019-06-21 11:20:47 -0400

    Se agregaron props al componente

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -5,6 +5,14 @@ import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component {
   render() {
+    const {
+      firstName,
+      lastName,
+      avatar,
+      jobTitle,
+      twitterUsername,
+      hashConf
+    } = this.props;
     return (
       <div className="Badge">
         <div className="Badge__header">
@@ -12,22 +20,18 @@ class Badge extends React.Component {
         </div>
 
         <div className="Badge__section-name">
-          <img
-            className="Badge__avatar"
-            src="https://es.gravatar.com/userimage/45568449/994578a2ded18412e6d69bfb0362823d?size=200"
-            alt="Avatar"
-          />
+          <img className="Badge__avatar" src={avatar} alt="Avatar" />
           <h1>
-            Mariangélica <br /> Useche
+            {firstName} <br /> {lastName}
           </h1>
         </div>
 
         <div className="Badge__section-info">
-          <h3>Computer Scientist</h3>
-          <div>@mustarte</div>
+          <h3>{jobTitle}</h3>
+          <div>@{twitterUsername}</div>
         </div>
 
-        <div className="Badge__footer">#platziconf</div>
+        <div className="Badge__footer">#{hashConf}</div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -7,4 +7,14 @@ import Badge from './components/Badge';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<Badge />, container);
+ReactDOM.render(
+  <Badge
+    firstName="Mariangélica"
+    lastName="Useche"
+    jobTitle="Computer Scientist"
+    twitterUsername="mustarte"
+    avatar="https://es.gravatar.com/userimage/45568449/994578a2ded18412e6d69bfb0362823d?size=200"
+    hashConf="platziconf"
+  />,
+  container
+);
